7d9d1cccc78c875073fba2bf58c74de4482512b8
TrineJA <trinejulie@gmail.com>
2021-01-27 21:26:20 +0100

    add emoji

--- app/vtx-lunch-menu-wizard.py
+++ app/vtx-lunch-menu-wizard.py
@@ -1,14 +1,20 @@
+import random 
+import requests
 import streamlit as st
 import sys
 sys.path.append("src")
-
 from get_menu import get_menu_as_string
 
+# get list of all emojis
+r = requests.get('https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json') 
+emoji_list = list(r.json().keys())
+
+# APP
 st.title('Welcome to the VTx lunch menu wizard!')
 
-get_menu = st.button('click here to get the latest menu')
-if get_menu:
-    menu = get_menu_as_string()
-    st.text(menu)
+menu = get_menu_as_string()
+st.text(menu)
 
+emoji = random.choice(emoji_list)
 st.markdown(f'Link to order takeaway: https://webshop.meyerskantiner.dk/shop/2240/take-away/g/23332')
+st.markdown(f'Have a nice day. Here is a random emoji :{emoji}:')
